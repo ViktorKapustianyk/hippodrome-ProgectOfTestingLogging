572aea29321dd58e22b074e7c5963bb71d4a5151
ViktorKapustianyk <vityakap@gmail.com>
2023-09-10 19:54:28 +0300

    Added class HippodromeTest add methods whick check constructor and all other methods.

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,116 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+
+class HippodromeTest {
+    @Test
+    @DisplayName("Check if parameter is null")
+    void checkIsNull() {
+        Throwable actualException = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Hippodrome(null);
+                }
+        );
+        assertEquals(IllegalArgumentException.class, actualException.getClass());
+    }
+
+    @Test
+    @DisplayName("Check Message parameter is null")
+    void checkMessageParameterIsNull() {
+        Throwable actualException = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Hippodrome(null);
+                }
+        );
+        assertEquals("Horses cannot be null.", actualException.getMessage());
+    }
+
+    @Test
+    @DisplayName("Check if parameter is empty list")
+    void checkIsEmptyList() {
+        Throwable actualException = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Hippodrome(Collections.emptyList());
+                }
+        );
+        assertEquals(IllegalArgumentException.class, actualException.getClass());
+    }
+
+    @Test
+    @DisplayName("Check Message if parameter is empty list")
+    void checkMessageIsEmptyList() {
+        Throwable actualException = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Hippodrome(Collections.emptyList());
+                }
+        );
+        assertEquals("Horses cannot be empty.", actualException.getMessage());
+    }
+
+    @Test
+    @DisplayName("Verify list contains same objects & same order.")
+    void getHorses() {
+        List<Horse> exceptedHorses = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            exceptedHorses.add(i, new Horse("Name" + i , i));
+        }
+        Hippodrome hippodrome = new Hippodrome(exceptedHorses);
+        List<Horse> actualHorses = hippodrome.getHorses();
+
+        assertIterableEquals(exceptedHorses, actualHorses);
+    }
+
+    @Test
+    @DisplayName("Check method move calls on all horses.")
+    void move() {
+        List<Horse> mockedHorses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            Horse horseMock = Mockito.mock(Horse.class);
+            mockedHorses.add(horseMock);
+        }
+        Hippodrome hippodrome = new Hippodrome(mockedHorses);
+        hippodrome.move();
+
+        for (Horse horseMock : mockedHorses) {
+            Mockito.verify(horseMock, times(1)).move();
+        }
+    }
+
+    @Test
+    @DisplayName("Verify method returns largest distance value.")
+    void getWinner() {
+        List<Horse> horses = new ArrayList<>();
+        Horse horse1 = new Horse("First", 3);
+        Horse horse2 = new Horse("Second", 2);
+        Horse horse3 = new Horse("Third", 1);
+
+        horses.add(horse1);
+        horses.add(horse2);
+        horses.add(horse3);
+
+        Hippodrome hippodrome = new Hippodrome(horses);
+        hippodrome.move();
+        Horse winner = hippodrome.getWinner();
+        double actualDistance = winner.getDistance();
+
+        for (Horse horse : horses) {
+            if (horse != winner) {
+                double horseDistance = horse.getDistance();
+                assertTrue(actualDistance > horseDistance);
+            }
+        }
+
+    }
+}
